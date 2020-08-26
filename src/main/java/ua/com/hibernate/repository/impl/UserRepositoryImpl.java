5506a2c0aced384bc2da42e61a16fd9be850d913
AndriiShykoriak <andrii.shykoriak@gmail.com>
2020-08-26 14:34:13 +0300

    crud user

--- src/main/java/ua/com/hibernate/repository/impl/UserRepositoryImpl.java
+++ src/main/java/ua/com/hibernate/repository/impl/UserRepositoryImpl.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import ua.com.hibernate.model.User;
 import ua.com.hibernate.repository.UserRepository;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -15,31 +14,26 @@ public class UserRepositoryImpl implements UserRepository {
     @Autowired
     SessionFactory SessionFactory;
 
-    @Transactional
     @Override
     public void addUser(User user) {
-        getCurrentSession().persist(user);
+        getCurrentSession().save(user);
     }
 
-    @Transactional
     @Override
     public void updateUser(User user) {
         getCurrentSession().update(user);
     }
 
-    @Transactional
     @Override
     public void deleteUser(User user) {
         getCurrentSession().delete(user);
     }
 
-    @Transactional
     @Override
     public User findById(int id) {
         return getCurrentSession().get(User.class, id);
     }
 
-    @Transactional
     @Override
     public List<User> findAll() {
         return getCurrentSession().createQuery("SELECT u from User  u", User.class).getResultList();
--- src/main/java/ua/com/hibernate/service/impl/UserServiceImpl.java
+++ src/main/java/ua/com/hibernate/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import ua.com.hibernate.repository.UserRepository;
 import ua.com.hibernate.repository.impl.UserRepositoryImpl;
 import ua.com.hibernate.service.UserService;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -14,26 +15,31 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Transactional
     @Override
     public void addUser(User user) {
         userRepository.addUser(user);
     }
 
+    @Transactional
     @Override
     public void updateUser(User user) {
         userRepository.updateUser(user);
     }
 
+    @Transactional
     @Override
     public void deleteUser(User user) {
         userRepository.deleteUser(user);
     }
 
+    @Transactional
     @Override
     public User findById(int id) {
         return userRepository.findById(id);
     }
 
+    @Transactional
     @Override
     public List<User> findAll() {
         return userRepository.findAll();
